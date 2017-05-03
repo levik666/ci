d276c4933b92a8aec24d8d2bbdaa57328274f74d
Yevgen Polukov <yevgen.polukov@paycash.onmicrosoft.com>
2017-05-03 23:32:42 +0300

    ls -la

--- merge.sh
+++ merge.sh
@@ -1,7 +1,8 @@
-echo 'checkotu release-candidate' && git checkout release-candidate/0.0.1 -f && git fetch && git pull origin release-candidate/0.0.1 -f &&
+ls -la && echo 'checkout release-candidate' && git checkout release-candidate/0.0.1 -f && git fetch && git pull origin release-candidate/0.0.1 -f &&
 echo 'switch to master' && git checkout master -f && git fetch && git pull origin master -f && 
 echo 'merge release-candidate to master' && git merge origin release-candidate/0.0.1 && git push origin master && 
-echo 'todo release process on master branch' && 
+echo 'todo release process on master branch' && ./gradlew :release -Prelease.useAutomaticVersion=true -Prelease.releaseVersion=0.10.16 -Prelease.newVersion=0.10.17-SNAPSHOT && 
 echo 'switch to develop' && git checkout develop -f && git fetch && git pull origin develop -f && 
 echo 'merge master to develop' && git merge origin master && git push origin develop &&
 echo 'release process finished'
+
