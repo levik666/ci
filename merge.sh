ae171d6e28a35de0e9c5a500497d3f7d5d5daacc
Yevgen Polukov <yevgen.polukov@paycash.onmicrosoft.com>
2017-05-04 17:53:42 +0300

    5

--- merge.sh
+++ merge.sh
@@ -5,22 +5,22 @@
 # Then reference then:
 echo "version is $version and prevTag is $prevTag and nextTag is $nextTag-SNAPSHOT"
 
-function string_replace {
-    echo "${1/\-/$2}"
+function string_replace() {
+    printf $version | sed 's/-SNAPSHOT$//'
 }
 
-$version=$(string_replace "$version" "SNAPSHOT")
+version_updated=$(string_replace "$version")
 
-if [ $version != $prevTag ]; then
-    echo "version is $version   should be equels with prevTag is $prevTag"
-    return -1;
+if [ "$version_updated" == "$prevTag" ]; then
+		# Release process:
+		pwd && ls -la && 
+		echo 'checkout release-candidate' && git checkout release-candidate/${version_updated} -f && git fetch && git pull origin release-candidate/${version_updated} -f &&
+		echo 'switch to master' && git checkout master -f && git fetch && git pull origin master -f && 
+		echo 'merge release-candidate to master' && git merge origin release-candidate/${version_updated} && git push origin master && 
+		echo 'todo release process on master branch' && ./gradlew :release -Prelease.useAutomaticVersion=true -Prelease.releaseVersion=${prevTag} -Prelease.newVersion=$nextTag-SNAPSHOT  && 
+		echo 'switch to develop' && git checkout develop -f && git fetch && git pull origin develop -f && 
+		echo 'merge master to develop' && git merge origin master && git push origin develop &&
+		echo 'release process finished'
+    else 
+    	echo "version is $version_updated should be equels with prevTag is $prevTag"
 fi
-
-ls -la && echo 'checkout release-candidate' && git checkout release-candidate/0.0.1 -f && git fetch && git pull origin release-candidate/0.0.1 -f &&
-echo 'switch to master' && git checkout master -f && git fetch && git pull origin master -f && 
-echo 'merge release-candidate to master' && git merge origin release-candidate/0.0.1 && git push origin master && 
-echo 'todo release process on master branch' && ./gradlew :release -Prelease.useAutomaticVersion=true -Prelease.releaseVersion=0.10.16 -Prelease.newVersion=0.10.17-SNAPSHOT && 
-echo 'switch to develop' && git checkout develop -f && git fetch && git pull origin develop -f && 
-echo 'merge master to develop' && git merge origin master && git push origin develop &&
-echo 'release process finished'
-
