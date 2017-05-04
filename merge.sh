9a8780fed7d6c4aa449bdf0b40e4f3a7c060f057
Yevgen Polukov <yevgen.polukov@paycash.onmicrosoft.com>
2017-05-04 08:18:23 +0300

    update sh

--- merge.sh
+++ merge.sh
@@ -1,3 +1,11 @@
+#!/bin/sh
+# source the properties:
+. gradle.properties
+
+# Then reference then:
+echo "version is $version and prevTag is $prevTag and nextTag is $nextTag-SNAPSHOT"
+
+
 ls -la && echo 'checkout release-candidate' && git checkout release-candidate/0.0.1 -f && git fetch && git pull origin release-candidate/0.0.1 -f &&
 echo 'switch to master' && git checkout master -f && git fetch && git pull origin master -f && 
 echo 'merge release-candidate to master' && git merge origin release-candidate/0.0.1 && git push origin master && 
